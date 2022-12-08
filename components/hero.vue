73861b2f889e8c7a28ca95f533796b1930e61734
balrumeys <106353330+balrumeys@users.noreply.github.com>
2022-12-08 10:48:33 +0300

    commit

---
+++ components/hero.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="video-banner" tabindex="4605">
+    <section class="hero-module mb-15">
+      <div class="row">
+        <div class="bg-wrapper">
+          <picture>
+            <source
+              height="552"
+              media="(min-width: 769px)"
+              srcset="
+                https://st-vans.mncdn.com/Content/img/Unisite/221118-HP-HERO-Raeburn-desk.jpg
+              "
+              width="1380"
+            />
+            <img
+              height="540"
+              rel="preload"
+              src="https://st-vans.mncdn.com/Content/img/Unisite/221118-HP-HERO-Raeburn-mob.jpg"
+              width="640"
+            />
+          </picture>
+        </div>
+
+        <div class="content-wrapper">
+          <div class="logo-wrapper">
+            <img
+              class=""
+              data-original="https://st-vans.mncdn.com/Content/img/Unisite/221118-HP-HERO-Raeburn-logo.png"
+              height="552"
+              media="(min-width: 768px)"
+              width="1380"
+              src="https://st-vans.mncdn.com/Content/img/Unisite/221118-HP-HERO-Raeburn-logo.png"
+              style=""
+            />
+          </div>
+
+          <div class="text-wrapper">
+            <h3>VANS X RÆBURN</h3>
+
+            <p>
+              A head-to-toe functional collection celebrating responsible
+              design.
+            </p>
+          </div>
+
+          <div class="buttons-wrapper">
+            <a class="button" href="/vans-x-reaburn">Alışverişe başla</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <Header />
+    <a href="/vans_iletisim">
+      <div id="vans-feedback-tab-area">ONLINE DESTEK</div>
+    </a>
+    <div id="container">
+      <div id="home">
+        <div class="w1380">
+          <Hero />
+          <Videobanner />
+          <Productbanner />
+          <Haberler />
+        </div>
+        <Koleksiyon />
+        <Homearea />
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+<script>
+import Header from "../components/header.vue";
+import Hero from "../components/hero.vue";
+import Videobanner from "../components/videobanner.vue";
+import Productbanner from "../components/productbanner";
+import Haberler from "../components/haberler.vue";
+import Koleksiyon from "../components/koleksiyon.vue";
+import Homearea from "../components/homearea.vue";
+import Footer from "../components/footer.vue";
+
+export default {
+  name: "IndexPage",
+  components: {
+    Header,
+    Hero,
+    Videobanner,
+    Productbanner,
+    Haberler,
+    Koleksiyon,
+    Homearea,
+    Footer,
+  },
+};
+</script>
